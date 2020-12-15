5ebe87ea8f12b8ade338b97d3386da259cc7b079
Jberov <jballer878@gmail.com>
2020-12-15 15:40:37 +0200

    Update AdministratorDBConnection.java

--- src/AdministratorDBConnection.java
+++ src/AdministratorDBConnection.java
@@ -6,6 +6,7 @@ public class AdministratorDBConnection extends DBConnectionHandler{
 
     public AdministratorDBConnection() {
         super();
+        setCompleteDBConnection ();
     }
     void setCompleteDBConnection(){
         connection = setDBConnection ();
